615f11502236b415b39227981a02d32a8f769379
ammaar12 <muhammadfarroos12@gmail.com>
2025-10-08 17:42:44 +0700

    feat: add home route and remove unused authentication controllers and views

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/home', function (){
+    return view('layouts.app2');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
