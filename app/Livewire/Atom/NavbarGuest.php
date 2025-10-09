da8b52740dd48933f271b910805d8c413b9612d0
ammaar12 <muhammadfarroos12@gmail.com>
2025-10-10 01:24:10 +0700

    feat: remove unused login view and add guest layout with navbar component

---
+++ app/Livewire/Atom/NavbarGuest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Atom;
+
+use Livewire\Component;
+
+class NavbarGuest extends Component
+{
+    public function render()
+    {
+        return view('livewire.atom.navbar-guest');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::get('/dashboard', function () {
 
 Route::get('/home2', Home::class)->name('home2');
 Route::get('/laporan', Home::class)->name('laporan');
+Route::get('/guest', function () {
+    return view('components.layouts.guest');
+})->name('guest');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
