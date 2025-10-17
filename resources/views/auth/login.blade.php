92af828b3c8a02dd0ac80a682a836a79930ff512
ammaar12 <muhammadfarroos12@gmail.com>
2025-10-17 13:02:02 +0700

    feat: implement admin dashboard layout and update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,36 +4,40 @@
 <div class="col-lg-5 col-12">
     <div id="auth-left">
         <div class="auth-logo">
-            <a href="index.html"><img src="./assets/compiled/svg/logo.svg" alt="Logo"></a>
+            <a href="index.html"><img src="images/adu.png" class="img-fluid" style="width: 160px; height: auto;" alt="Logo"></a>
         </div>
         <h1 class="auth-title">Log in.</h1>
-        <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+        <p class="auth-subtitle mb-5">Login menggunakan akun yang sudah teregister / terdaftar</p>
 
-        <form action="index.html">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
             <div class="form-group position-relative has-icon-left mb-4">
-                <input type="text" class="form-control form-control-xl" placeholder="Username">
+                <input type="email" name="email" class="form-control
+                @error('email')
+                is-invalid
+                @enderror form-control-xl" placeholder="Email">
                 <div class="form-control-icon">
                     <i class="bi bi-person"></i>
                 </div>
+                @error('email')
+                <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
             <div class="form-group position-relative has-icon-left mb-4">
-                <input type="password" class="form-control form-control-xl" placeholder="Password">
+                <input type="password"  name="password" class="form-control form-control-xl @error('password')
+                is-invalid
+                @enderror " placeholder="Password">
                 <div class="form-control-icon">
                     <i class="bi bi-shield-lock"></i>
                 </div>
+                @error('password')
+                <small class="text-danger">{{ $message }}</small>
+                @enderror
             </div>
-            <div class="form-check form-check-lg d-flex align-items-end">
-                <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                <label class="form-check-label text-gray-600" for="flexCheckDefault">
-                    Keep me logged in
-                </label>
-            </div>
-            <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+            <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-1">Log in</button>
         </form>
         <div class="text-center mt-5 text-lg fs-4">
-            <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
-                    up</a>.</p>
-            <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
+            {{-- <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p> --}}
         </div>
     </div>
 </div>
