dbf7fa28ad0fb760b7c94c5bdc5e82286cf9d5aa
ammaar12 <muhammadfarroos12@gmail.com>
2025-11-13 03:01:24 +0700

    feat: add complaint report form and update guest view with role-based access

--- resources/views/livewire/guest/complaint-form.blade.php
+++ resources/views/livewire/guest/complaint-form.blade.php
@@ -1,74 +1,19 @@
-@section('title','Form Pengaduan')
-<div>
-
-    @if (Auth::check() && Auth::user()->role == 'admin')
-    <div class="page-heading">
-        <h3>Anda Admin</h3>
-        <p>Tidak bisa melakukan laporan</p>
-    </div>
-    @else
-    <div class="page-heading">
-        <h3>Form Pengaduan Masyarakat</h3>
-    </div>
-        <section id="multiple-column-form">
-            <div class="row match-height">
-                <div class="col-12">
-                    <div class="card shadow-sm">
-                        <div class="card-header text-center text-uppercase bg-primary text-white">
-                            <h4 class="card-title">Masukkan Informasi Pengaduan</h4>
-                        </div>
-                        <div class="card-content">
-                            <div class="card-body">
-                                <form class="form">
-                                    <div class="row">
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="first-name-column">Nama Lengkap</label>
-                                                <input type="text" id="first-name-column" class="form-control" placeholder="Nama Lengkap" name="fname-column" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="country-floating">Judul Pengaduan</label>
-                                                <input type="text" id="country-floating" class="form-control" name="country-floating" placeholder="Judul Pengaduan" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="last-name-column">Nomor Telepon</label>
-                                                <input type="text" id="last-name-column" class="form-control" placeholder="Nomor Telepon" name="lname-column" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="company-column">Gambar</label>
-                                                <input type="file" id="company-column" class="form-control" name="company-column" placeholder="Image" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="city-column">Alamat Email</label>
-                                                <input type="email" id="city-column" class="form-control" placeholder="Alamat Email" name="city-column" required>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6 col-12 mb-3">
-                                            <div class="form-group">
-                                                <label for="email-id-column">Deskripsi</label>
-                                                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" required></textarea>
-                                            </div>
-                                        </div>
-                                        <div class="col-12 d-flex justify-content-end">
-                                            <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
-                                            <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+@section('title', 'Form Pengaduan')
+<div class="d-flex flex-column" style="min-height: 65vh">
+    @auth
+        @if (Auth::user()->role == 'admin')
+            <div class="page-heading">
+                <h3>Anda Admin</h3>
+                <p>Tidak bisa melakukan laporan</p>
             </div>
-        </section>
-    @endif
+        @else
+            <livewire:user.create-report />
+        @endif
+    @else
+        {{-- <div class="text-center"> --}}
+        <div class="flex-grow-1 d-flex align-items-center justify-content-center">
+            <a href="{{ route('login') }}" class="btn btn-primary">Login untuk membuat laporan</a>
+        </div>
+    @endauth
 
 </div>
--- resources/views/livewire/guest/statistica.blade.php
+++ resources/views/livewire/guest/statistica.blade.php
@@ -1,12 +1,12 @@
 @section('title', 'Data Statistik Pengaduan')
 
-<div>
+<div class="d-flex flex-column" style="min-height: 65vh">
     <div class="page-heading">
         <h3>Laporan Jumlah Pengaduan</h3>
     </div>
     <div class="page-content">
         <section class="row">
-            <div class="col-12 col-lg-12">
+            <div class="col-12 col-lg-9">
                 <div class="row">
                     <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
@@ -18,8 +18,8 @@
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Total Pengaduan</h6>
-                                        <h6 class="font-extrabold mb-0">112.000</h6>
+                                        <h6 class="text-muted font-semibold">Total Laporan</h6>
+                                        <h6 class="font-extrabold mb-0">{{$totalReports}}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -36,7 +36,7 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Pending</h6>
-                                        <h6 class="font-extrabold mb-0">183.000</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $pendingReports }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -53,7 +53,7 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Diproses</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $inProcessReports }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -70,43 +70,96 @@
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
                                         <h6 class="text-muted font-semibold">Selesai</h6>
-                                        <h6 class="font-extrabold mb-0">112</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $completedReports }}</h6>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>Laporan Pengaduan Per Bulan {{date('Y')}}</h4>
+                            </div>
+                            <div class="card-body">
+                                <div id="grafik-laporan-bar"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-lg-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Laporan Per Kategori</h4>
+                    </div>
+                    <div class="card-body">
+                        <div id="grafik-laporan-pie"></div>
+                    </div>
+                </div>
             </div>
         </section>
-        <div class="card">
-            <div class="card-header">
-                <h4>Statistik Pengaduan</h4>
-            </div>
-            <div class="card-body">
-                <div id="chart-pengaduan"></div>
-            </div>
-        </div>
     </div>
 </div>
 
 @section('scripts')
     <script src="{{asset('mazer/assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
     <script>
-        var options = {
-            series: [30, 80, 25],
-            chart: {
-                type: 'pie',
-                height: 350
-            },
-            labels: ['Pending', 'Diproses', 'Selesai'],
-            colors: ['#ff7976', '#57caeb', '#5ddab4'],
-            legend: {
-                position: 'bottom',
-                horizontalAlign: 'center'
+        // var options = {
+        //     series: [30, 80, 25],
+        //     chart: {
+        //         type: 'pie',
+        //         height: 350
+        //     },
+        //     labels: ['Pending', 'Diproses', 'Selesai'],
+        //     colors: ['#ff7976', '#57caeb', '#5ddab4'],
+        //     legend: {
+        //         position: 'bottom',
+        //         horizontalAlign: 'center'
+        //     }
+        // };
+        // var chart = new ApexCharts(document.querySelector("#chart-pengaduan"), options);
+        // chart.render();
+
+    let laporanPerBulan = @json($monthlyReport);
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'];
+
+    new ApexCharts(document.querySelector("#grafik-laporan-bar"), {
+        series: [{
+            name: 'Laporan',
+            data: months.map((m,i) => laporanPerBulan[String(i+1).padStart(2, '0')] ?? 0)
+        }],
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        xaxis: {
+            categories: months
+        }
+    }).render();
+
+    new ApexCharts(document.querySelector("#grafik-laporan-pie"), {
+        series: [<?= $pendingReports ?>, <?= $inProcessReports ?>, <?= $completedReports ?>],
+        labels: ['Pending', 'Diproses', 'Selesai'],
+        colors: ['#ff7976', '#57caeb', '#5ddab4'],
+        chart: {
+            type: 'pie',
+            height: 350
+        },
+        plotOptions: {
+            pie: {
+                donut: {
+                    size: '70%'
+                }
             }
-        };
-        var chart = new ApexCharts(document.querySelector("#chart-pengaduan"), options);
-        chart.render();
+        },
+        legend: {
+            position: 'bottom'
+        }
+    }).render();
+
+
     </script>
 @endsection
