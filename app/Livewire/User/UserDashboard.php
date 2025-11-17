f112bede44293e3b6204f952375d052ea6b98274
ammaar12 <muhammadfarroos12@gmail.com>
2025-11-17 16:14:12 +0700

    feat: enhance user dashboard with report statistics and updated UI elements

--- app/Livewire/User/UserDashboard.php
+++ app/Livewire/User/UserDashboard.php
@@ -2,10 +2,21 @@
 
 namespace App\Livewire\User;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class UserDashboard extends Component
 {
+    public $totalLaporan, $laporanPending, $laporanDiproses, $laporanSelesai;
+    public function mount(){
+        $user_id = Auth::user()->id;
+
+        $this->totalLaporan = \App\Models\Report::where('reporter_id', $user_id)->count();
+        $this->laporanPending = \App\Models\Report::where('reporter_id', $user_id)->where('status', 'pending')->count();
+        $this->laporanDiproses = \App\Models\Report::where('reporter_id', $user_id)->where('status', 'proses')->count();
+        $this->laporanSelesai = \App\Models\Report::where('reporter_id', $user_id)->where('status', 'selesai')->count();
+    }
+
     public function render()
     {
         return view('livewire.user.user-dashboard');
--- resources/views/livewire/user/user-dashboard.blade.php
+++ resources/views/livewire/user/user-dashboard.blade.php
@@ -10,12 +10,12 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="stats-icon purple mb-2">
-                                            <i class="iconly-boldShow"></i>
+                                            <i class="bi-chat"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Profile Views</h6>
-                                        <h6 class="font-extrabold mb-0">112.000</h6>
+                                        <h6 class="text-muted font-semibold">Total Pengaduan</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $totalLaporan }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -27,12 +27,12 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="stats-icon blue mb-2">
-                                            <i class="iconly-boldProfile"></i>
+                                            <i class="bi-hourglass"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Followers</h6>
-                                        <h6 class="font-extrabold mb-0">183.000</h6>
+                                        <h6 class="text-muted font-semibold">Laporan Pending</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $laporanPending }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -44,12 +44,12 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="stats-icon green mb-2">
-                                            <i class="iconly-boldAdd-User"></i>
+                                            <i class="bi-arrow-repeat"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Following</h6>
-                                        <h6 class="font-extrabold mb-0">80.000</h6>
+                                        <h6 class="text-muted font-semibold">Laporan Proses</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $laporanDiproses }}</h6>
                                     </div>
                                 </div>
                             </div>
@@ -61,12 +61,12 @@
                                 <div class="row">
                                     <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start ">
                                         <div class="stats-icon red mb-2">
-                                            <i class="iconly-boldBookmark"></i>
+                                            <i class="bi-check-circle"></i>
                                         </div>
                                     </div>
                                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                        <h6 class="text-muted font-semibold">Saved Post</h6>
-                                        <h6 class="font-extrabold mb-0">112</h6>
+                                        <h6 class="text-muted font-semibold">Laporan selesai</h6>
+                                        <h6 class="font-extrabold mb-0">{{ $laporanSelesai }}</h6>
                                     </div>
                                 </div>
                             </div>
